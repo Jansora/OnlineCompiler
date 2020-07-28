ee5c610a0604febf659c3613bdc6feb9fd4ad2f4
Jansora <18044846@suning.com>
2020-07-28 10:27:27 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 9002:9002 --name onlinecompiler -v /home/ubuntu/docker/OnlineCompiler/data:/app/data onlinecompiler:v2
+sudo docker run -d -p 9002:9002 --name onlinecompiler -v /home/ubuntu/docker/OnlineCompiler/data:/app/data jansora/onlinecompiler:v2
