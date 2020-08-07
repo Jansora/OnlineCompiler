8e5669e58026e0a50f60ce895ad2be987c27b9d2
Jansora <18044846@suning.com>
2020-08-07 11:22:01 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 9002:9003 --name onlinecompiler -v /home/ubuntu/docker/OnlineCompiler/data:/app/data jansora/onlinecompiler:v4
+sudo docker run -d -p 9002:9003 --name onlinecompiler -v /home/ubuntu/docker/github/OnlineCompiler/frontend/build:/app/frontend.build -v /home/ubuntu/docker/OnlineCompiler/data:/app/data jansora/onlinecompiler:v4
