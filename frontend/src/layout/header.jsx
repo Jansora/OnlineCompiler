5d142d1fe3e106a218d15c115cafa594d9a29255
Jansora <18044846@suning.com>
2020-08-07 11:06:47 +0800

    optimize sql result

--- frontend/src/layout/header.jsx
+++ frontend/src/layout/header.jsx
@@ -22,7 +22,7 @@ const Header = (props) => {
               <NavLink to="/go">Golang</NavLink>
               <NavLink to="/javascript">Browser Javascript</NavLink>
               <NavLink to="/node">Nodejs</NavLink>
-              <NavLink to="/sql">SQL(Base On SQLite)</NavLink>
+              <NavLink to="/sql">SQL(SQLite)</NavLink>
 
             </StyledHeaderLeft>
           <a target="_blank" rel='noopener noreferrer' href={"https://github.com/Jansora/OnlineCompiler"} style={{float: "right"}}>Github</a>
--- frontend/src/layout/renderSqlResult.jsx
+++ frontend/src/layout/renderSqlResult.jsx
@@ -1,38 +1,42 @@
 import React from "react";
-import {Icon, Table} from "semantic-ui-react";
+import {Tab, Table} from "semantic-ui-react";
 
 const RenderSqlResult = (result) => {
 
   if (!(result instanceof Array)) return ''
+
+  const panes = result.map(({columns, values}, index) => {
+    return {
+      menuItem: `SELECT Result ${index}`,
+      render: () => <>
+        <Table celled striped  compact='very'>
+          <Table.Header>
+            <Table.Row>
+              {
+                columns.map(column => <Table.HeaderCell>{column}</Table.HeaderCell>)
+              }
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {
+              values.map(value => <Table.Row>
+                {
+                  value.map( v => <Table.Cell>{v}</Table.Cell>
+                  )
+                }
+              </Table.Row>)
+            }
+          </Table.Body>
+        </Table>
+      </>,
+    }
+  });
+
+
   return (
     <React.Fragment>
-      {
-        result.map(
-          ({columns, values}, index) => <Table celled striped  compact='very'>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell colSpan={columns.length.toString()}>SELECT Result {index}</Table.HeaderCell>
-              </Table.Row>
-              <Table.Row>
-                {
-                  columns.map(column => <Table.HeaderCell>{column}</Table.HeaderCell>)
-                }
-              </Table.Row>
-            </Table.Header>
-
-            <Table.Body>
-              {
-                values.map(value => <Table.Row>
-                  {
-                    value.map( v => <Table.Cell>{v}</Table.Cell>
-                    )
-                  }
-                </Table.Row>)
-              }
-            </Table.Body>
-          </Table>
-        )
-      }
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
     </React.Fragment>
   )
 }
